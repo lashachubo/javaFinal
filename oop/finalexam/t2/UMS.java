634ab9262e999edc99875ca1b78b6f01a7a448fd
lashachubo <lashachubinidz3@gmail.com>
2025-07-22 19:00:09 +0400

    clean up

--- oop/finalexam/t2/UMS.java
+++ oop/finalexam/t2/UMS.java
@@ -19,7 +19,7 @@ public class UMS {
     }
 
     public void printStudentData(Student student) {
-        System.out.println("Student Name:" + student.getName());
+        System.out.println("Student Name: " + student.getName());
         System.out.println("Learning Courses:");
         for (LearningCourse course : student.getLearningCourses()) {
             System.out.println(" - " + course);
@@ -54,20 +54,5 @@ public class UMS {
         return lasha;
     }
 
-    public static void main(String[] args) {
-        UMS ums = new UMS();
 
-        Student lasha = ums.getMyStudentData();
-        ums.addStudent(lasha);
-
-        Student student2 = new Student("Lionel Messi");
-        student2.addCourse(new LearningCourse("Calculus 101", "None", "Algebra"));
-        student2.addCourse(new LearningCourse("Introduction to C", "None", "C, loops, lists"));
-        student2.addCourse(new LearningCourse("Physics 101", "None", "Physics"));
-        ums.addStudent(student2);
-
-        for (Student s : ums.getStudents()) {
-            ums.printStudentData(s);
-        }
-    }
 }
